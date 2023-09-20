75e584a4a3d4ca35929c384a68ee582bbda1ade7
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-09-20 13:33:09 -0700

    simple shell project

---
+++ shell2.c
@@ -0,0 +1,108 @@
+#include"shell.h"
+/**
+ * _myhistory - displays the history line
+ * @info: Structure containing potential arguments
+ *  Return: 0
+ */
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
+
+/**
+ * unset_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ * Return: 0 or 1
+ */
+int unset_alias(info_t *info, char *str)
+{
+	char *ptr, a;
+	int mul;
+
+	ptr = _strchr(str, '=');
+	if (!ptr)
+		return (1);
+	a = *ptr;
+	*ptr = 0;
+	mul = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+	*ptr = a;
+	return (mul);
+}
+
+/**
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ * Return: 0 or 1
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *ptr;
+
+	ptr = _strchr(str, '=');
+	if (!ptr)
+		return (1);
+	if (!*++ptr)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints an alias string
+ * @node: the alias node
+ * Return: 0 or 1
+ */
+int print_alias(list_t *node)
+{
+	char *ptr = NULL, *a = NULL;
+
+	if (node)
+	{
+		ptr = _strchr(node->str, '=');
+		for (a = node->str; a <= ptr; a++)
+		_putchar(*a);
+		_putchar('\'');
+		_puts(ptr + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin
+ * @info: Structure containing potential arguments
+ *  Return: 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *ptr = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		ptr = _strchr(info->argv[i], '=');
+		if (ptr)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
